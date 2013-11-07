83b821a1a5013c10a58ba3cf6819d3ac23b575bf
ctfysh <ctfysh@163.com>
2013-11-07 10:09:20 +0800

    A little change for the help documentation.

--- R/accessfun.r
+++ R/accessfun.r
@@ -5,8 +5,8 @@
 #' This function is used to separete the SQL codes into several single ones
 #' which can be easily called by Mircrosoft Access with R package RODBC.
 #' 
-#' @param file the filename of SQL codes
-#' @return A string vector with several single SQL codes
+#' @param file the filename of SQL codes.
+#' @return A string vector with several single SQL codes.
 #' @export
 #' 
 querysep=function(file){
@@ -30,27 +30,30 @@ querysep=function(file){
 #' A new *.mdb database will be created by copying the "EmptyDB.mdb" file with
 #' your database name.
 #' 
-#' @param dbname the database name which you create
-#' @param emptydb the empty database you will copy
-#' @param dir the directory of your new created database
-#' @param ... arguments to be passed to \code{\link{file.copy}}
-#' @return A new *.mdb database
+#' @param dbname the database name which you create.
+#' @param extension the extension of the database.
+#' @param emptydb the empty database you will copy.
+#' @param dir the directory of your new created database.
+#' @param ... arguments to be passed to \code{\link{file.copy}}.
+#' @return A new *.mdb database.
 #' @export
 #' 
-createmdb=function(dbname="newdb.mdb",
-                   emptydb=system.file("EmptyDB.mdb",package="rccess"),
+createdb=function(dbname="newdb",extension=".mdb",
+                   emptydb=system.file(
+                       paste("emptydb/EmptyDB",extension,sep=""),
+                       package="rccess"),
                    dir=getwd(),...){
-    file.copy(emptydb,paste(dir,dbname,sep="/"),...)
+    file.copy(emptydb,paste(dir,"/",dbname,extension,sep=""),...)
 }
 
 #' Call with Batch SQL Codes
 #' 
 #' Submit each SQL query to a *.mdb database, and retrieve the results.
 #' 
-#' @param channel connection handle as returned by \code{\link{odbcConnect}}
-#' @param query any valid SQL statement
-#' @param ... arguments to be passed to \code{\link{sqlQuery}}
-#' @return Values are same with \code{\link{sqlQuery}}
+#' @param channel connection handle as returned by \code{\link{odbcConnect}}.
+#' @param query any valid SQL statement.
+#' @param ... arguments to be passed to \code{\link{sqlQuery}}.
+#' @return Values are same with \code{\link{sqlQuery}}.
 #' @export
 #' 
 sqlQuerys=function(channel,queries,...){
@@ -61,11 +64,11 @@ sqlQuerys=function(channel,queries,...){
 #' 
 #' Create a list of the fields in each table.
 #' 
-#' @param channel connection handle as returned by \code{\link{odbcConnect}}
+#' @param channel connection handle as returned by \code{\link{odbcConnect}}.
 #' @param tableType specify zero or more types in separate elements of a 
-#' character vector
-#' @param ... arguments to be passed to \code{\link{file.copy}}
-#' @return A list of the fields in each table will be returned
+#' character vector.
+#' @param ... arguments to be passed to \code{\link{file.copy}}.
+#' @return A list of the fields in each table will be returned.
 #' @export
 #' 
 sqlFields=function(channel,tableType="TABLE",...){
--- R/rccess-package.r
+++ R/rccess-package.r
@@ -1,5 +1,8 @@
 #' rccess
+#' 
+#' rccess is a extension of the RODBC package.
 #'
 #' @name rccess
 #' @docType package
+#' @author Hu Sheng
 NULL
